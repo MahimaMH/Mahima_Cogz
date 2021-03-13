5578a01bcb4f8ff799e8c65263e6d0258186a684
MahimaMH <mahimamh16@gmail.com>
2021-03-13 17:45:02 +0530

    Add programe

---
+++ Doller.java
@@ -0,0 +1,12 @@
+/**
+* Name : Mahima
+* Description : Rupee to doller
+* Date : 12/3/21
+*/
+class Doller{
+	public static void main(String[] args) {
+	double R=199;
+	double D=R/72.83;
+	System.out.println("Doller:" +D);
+	}
+	}
